4b6a442fb8499604cba5856125b5b32a2c19125e
24기구원 <koowonqwe2@gmail.com>
2023-10-12 01:27:50 +0900

    .

--- Snowball/Assets/Scripts/move.cs
+++ Snowball/Assets/Scripts/move.cs
@@ -8,4 +8,44 @@ public class move : MonoBehaviour
     Rigidbody2D rm;
     bool leftpress = false;
     bool rightpress = false;
+
+    public void Start()
+    {
+        rm = GetComponent<Rigidbody2D>();
+    }
+
+     void Update()
+    {
+        if(rm!= null)
+        {
+            float dist=SPEED_move*Time.deltaTime;
+            Vector2 pos = transform.position;
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                leftpress = true;
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                leftpress = false;
+            }
+            if(leftpress)
+            {
+                pos.x -= dist;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rightpress = true;  
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                rightpress = false;
+            }
+            if(rightpress)
+            {
+                pos.x += dist;
+            }
+            transform.position = pos;
+        }
+        
+    }
 }
