8c053edb286093e38669cf9683dcca1a08bfffe7
24기구원 <koowonqwe2@gmail.com>
2023-10-12 01:18:23 +0900

    .

---
+++ Snowball/Assets/Scripts/move.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    const float SPEED_move=3.0f;
+    Rigidbody2D rm;
+    bool leftpress = false;
+    bool rightpress = false;
+}
