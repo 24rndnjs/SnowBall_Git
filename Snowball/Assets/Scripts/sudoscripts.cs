b695779e3819dc028b4888ca2357cfb12d3b421a
starsol1234 <128784224+Leehansol1234@users.noreply.github.com>
2023-10-13 00:59:26 +0900

    asfd

---
+++ Snowball/Assets/Scripts/sudoscripts.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sudoscripts : MonoBehaviour
+{
+    public GameObject Target1;
+    public GameObject Target2;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Target1.SetActive(true);
+        Target2.SetActive(true);
+    }
+}
