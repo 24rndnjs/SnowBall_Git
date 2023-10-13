6fb0931357b4d5f715e8b946a11b6640fb013e75
1403wlaud <jeemyung1129@gmail.com>
2023-10-13 20:47:43 +0900

    sssssssssssssssssssss

--- Snowball/Assets/Scripts/sudoscripts.cs
+++ Snowball/Assets/Scripts/sudoscripts.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sudoscripts : MonoBehaviour
 {
     public GameObject Target1;
     public GameObject Target2;
-
+    public GameObject OffSuDoBtn;
+    public GameObject OnMirrorTriger;
+    public Text Sucses;
     void OnTriggerEnter2D(Collider2D other)
     {
         Target1.SetActive(true);
@@ -16,5 +19,12 @@ public class sudoscripts : MonoBehaviour
     {
         Target1.SetActive(false);
         Target2.SetActive(false);
+    }
+
+    public void Take_a_ShowerBtnOnclick()
+    {
+        Sucses.text = "- ¾Ä±â ¼º°ø! -";
+        OffSuDoBtn.SetActive(false);
+        OnMirrorTriger.SetActive(true);
     }
 }
