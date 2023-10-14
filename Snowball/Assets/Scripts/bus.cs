11e7400548dc634f87a7cce6421e4e8d98d454a2
24기구원 <koowonqwe2@gmail.com>
2023-10-14 18:12:36 +0900

    bus scene making

--- Snowball/Assets/Scripts/bus.cs
+++ Snowball/Assets/Scripts/bus.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class bus : MonoBehaviour
 {
+    public float _Speed; // 버스의 이동 속도
+    private Vector3 _direct = Vector3.zero;
+    private GameObject _targetObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _direct = new Vector3(-1.0f, 1.0f, 0.0f).normalized;
+        _targetObj = GameObject.Find("Target");
+    }
+
+    private void Move1()
+    {
+        this.transform.position += new Vector3(0.0f, 0.01f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        this.transform.position = Vector3.MoveTowards(this.transform.position, _targetObj.transform.position, _Speed * Time.deltaTime);
     }
 }
