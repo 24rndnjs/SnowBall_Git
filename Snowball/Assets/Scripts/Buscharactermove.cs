2578c47c475314bb286549a7c6a33a3f85bee54d
24기구원 <koowonqwe2@gmail.com>
2023-10-16 02:18:11 +0900

    move

---
+++ Snowball/Assets/Scripts/Buscharactermove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buscharactermove : MonoBehaviour
+{
+    [SerializeField][Range(1f, 10f)] float moveSpeed = 3f;
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector3 flipMove = Vector3.zero;
+
+        if (horizontalInput < 0)
+        {
+            flipMove = Vector3.left;
+            transform.localScale = new Vector3(0.190975f, 0.55182f, 0.6161985f);
+        }
+        else if (horizontalInput > 0)
+        {
+            flipMove = Vector3.right;
+            transform.localScale = new Vector3(-0.190975f, 0.55182f, 0.6161985f);
+        }
+
+        transform.position += flipMove * moveSpeed * Time.deltaTime;
+
+        // 걷기 애니메이션 제어
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
