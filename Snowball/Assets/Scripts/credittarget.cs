ffb1ef3919abf2fe416ec8c1d558a9184a1c8d32
24기구원 <koowonqwe2@gmail.com>
2023-10-17 23:17:44 +0900

    credit

---
+++ Snowball/Assets/Scripts/credittarget.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class credittarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float _Speed; //¼Óµµ
+    private Vector3 _direct = Vector3.zero;
+    private GameObject _targetObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _direct = new Vector3(-1.0f, 1.0f, 0.0f).normalized;
+        _targetObj = GameObject.Find("last");
+    }
+
+    private void Move1()
+    {
+        this.transform.position += new Vector3(0.0f, 0.01f, 0.0f);
+    }
+    private void Update()
+    {
+
+        if (_targetObj != null)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, _targetObj.transform.position, _Speed * Time.deltaTime);
+        }
+    }
+}
