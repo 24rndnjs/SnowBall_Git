3ca2ddbb5c094dcf7849bf4f102e41d90d7c7cfe
1403wlaud <jeemyung1129@gmail.com>
2023-10-18 02:09:02 +0900

    sss

--- Snowball/Assets/Scripts/BusCOPYBtnManager.cs
+++ Snowball/Assets/Scripts/BusCOPYBtnManager.cs
@@ -7,6 +7,7 @@ public class BusCOPYBtnManager : MonoBehaviour
     public GameObject Jotrigeroff;
     public GameObject TalkImg;
     public GameObject BusSpriteoff;
+    public GameObject OnBusTriger;
     public GameObject obj;
 
     public void JoBtnOnclick()
@@ -14,6 +15,7 @@ public class BusCOPYBtnManager : MonoBehaviour
         Jotrigeroff.SetActive(false);
         TalkImg.SetActive(true);
         BusSpriteoff.SetActive(false);
+        OnBusTriger.SetActive(true);
         obj.GetComponent<Buscharactermove>().enabled = false;
     }
 }
